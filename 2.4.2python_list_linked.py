dfb13800fde8b1dce3b6b1c85f2701f6a397b65c
mototsugu ohgami <gamix255@gmail.com>
2015-01-13 12:54:39 +0900

    p52 part 3 list + list

---
+++ 2.4.2python_list_linked.py
@@ -0,0 +1,16 @@
+
+"""
+this test check by Python 2.7.6 (ubuntu 14.04)
+
+Python list information. part 3, linking. For example,
+
+>>> a = [2, 3]
+>>> b = [5, 6]
+>>> print a + b
+[2, 3, 5, 6]
+>>>
+"""
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
