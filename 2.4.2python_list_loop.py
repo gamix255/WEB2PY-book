58226bb23c491cd6f587e764a5e55839a5c4fa30
mototsugu ohgami <gamix255@gmail.com>
2015-01-14 12:59:30 +0900

    p52 python list looping sample

---
+++ 2.4.2python_list_loop.py
@@ -0,0 +1,20 @@
+
+"""
+this test check by Python 2.7.6 (ubuntu 14.04)
+
+Python list information. part 4, looping. For example,
+
+>>> a = [1, 2, 3]
+>>> for i in a:
+...     print i
+...
+1
+2
+3
+>>>
+"""
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
+
