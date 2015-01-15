688bfa4fdb580167bcecd5a2b8a3be8df853c832
mototsugu ohgami <gamix255@gmail.com>
2015-01-15 23:59:38 +0900

    p53

---
+++ 2.4.2python_list_comprehension.py
@@ -0,0 +1,21 @@
+
+"""
+this test check by Python 2.7.6 (ubuntu 14.04)
+
+Python list information. part 5, comprehension. For example,
+
+>>> a = [1, 2, 3, 4, 5]
+>>> b = []
+>>> for x in a:
+...    if x % 2 == 0:
+...       b.append(x * 3)
+...
+>>> b
+[6, 12]
+
+>>> 
+"""
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
