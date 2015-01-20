89e62cdb2d96edb2778035a3f2be4257a79d350c
mototsugu ohgami <gamix255@gmail.com>
2015-01-20 12:54:49 +0900

    p55 about hash()

---
+++ 2.4.4python_hash.py
@@ -0,0 +1,13 @@
+
+"""
+this test check by Python 2.7.6 (ubuntu 14.04)
+
+Python dict information. For example,
+
+>>> hash ("hello world")
+>>> -1500746465
+#pythonのオブジェクトの場所はhash関数でわかる。けれど、ランダマイズ
+されたので、3.3以降では都度変わります。
+http://docs.python.jp/3.3/whatsnew/3.3.html#builtin-functions-and-types
+"""
+
