a5b351f8c4e9954d005cc9e38784aaec7f4ee580
mototsugu ohgami <gamix255@gmail.com>
2015-01-20 12:58:39 +0900

    p52 indent

---
+++ 2.5python_indent.py
@@ -0,0 +1,20 @@
+
+"""
+this test check by Python 2.7.6 (ubuntu 14.04)
+
+Python indent information. For example,
+
+>>> i = 0
+>>> while i < 3:
+...     print i
+...     i = i + 1
+...
+0
+1
+2
+>>>
+"""
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
