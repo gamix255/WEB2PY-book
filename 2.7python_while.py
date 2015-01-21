2fd86e18a4b02f3794f74ae299ada1c0c2382f57
mototsugu ohgami <gamix255@gmail.com>
2015-01-21 12:56:06 +0900

    p57 python while

---
+++ 2.7python_while.py
@@ -0,0 +1,21 @@
+
+"""
+this test check by Python 2.7.6 (ubuntu 14.04)
+
+Python while information. For example,
+memo: and python have not loop...until notation
+
+>>> i = 0
+>>> while i < 10:
+...    i = i + 1
+...
+>>> print i
+10
+
+
+"""
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
+
