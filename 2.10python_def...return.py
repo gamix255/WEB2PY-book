eda77473702d49465e9760334a182e8fab680db6
mototsugu ohgami <gamix255@gmail.com>
2015-01-25 08:12:12 +0900

    p60 def return

---
+++ 2.10python_def...return.py
@@ -0,0 +1,18 @@
+
+"""
+this test check by Python 2.7.6 (ubuntu 14.04)
+
+Python def...return information. For example,
+
+>>> def f(a, b):
+...     return a + b
+... 
+>>> print f(4, 2)
+6
+
+"""
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
+
